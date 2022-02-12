991db6cb0ed88d392dc9375ef8e371117a06e4fd
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2022-02-12 16:37:51 +0900

    first commit

---
+++ Minesweeper.py
@@ -0,0 +1,18 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while True:
+    for i in range(16):
+        for cell in input().split():
+            # cell: '?' if unknown, '.' if no mines nearby, '1'-'8' otherwise
+            pass
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+
+    print("20 7")
