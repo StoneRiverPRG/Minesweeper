e91325d0ed50860a3277ee10dab1ddc07a84460f
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2022-02-12 23:12:29 +0900

    クラス化完了

--- Minesweeper.py
+++ Minesweeper.py
@@ -10,7 +10,7 @@ import math
 # the standard input according to the problem statement.
 
 class GameState(Enum):
-    """GameState minesweeper state
+    """GameState minesweeper Gaming state
 
     Args:
         enum ([type]): [description]
@@ -20,7 +20,7 @@ class GameState(Enum):
     BOMB = auto() # mine explosion(Game end)
 
 
-class mine():
+class Mine():
     """mine minesweeper program.
     Todo:
     """
@@ -29,27 +29,53 @@ class mine():
         self.WIDTH = 30
         self.HEIGHT = 16
         self.MINEMAX = 99
-
-width = 30
-height = 16
-mineCount = 99
-
-
-# game loop
-while True:
-    mine_map = [[] for _ in range(height)]
-    for i in range(height):
-        for cell in input().split():
-            # cell: '?' if unknown, '.' if no mines nearby, '1'-'8' otherwise
-            mine_map[i].append(cell)
-            # print(cell, file=sys.stderr, end="")
-        # print("", file=sys.stderr)
-
-    for line in mine_map:
-        print("".join(line), file=sys.stderr)
+        self.MineCount = self.MINEMAX
+        self.MineMap = [[] for _ in range(self.HEIGHT)]
+        self.GameState = GameState.GAME
+        self.Turn = 0
 
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    def print_Map(self):
+        """print_Map display Mine Map
+        """
+        print(f"turn {self.Turn}", file=sys.stderr)
+        for row in self.MineMap:
+            print("".join(row), file=sys.stderr)
 
-    print("20 7")
+
+    def input_data(self):
+        # map initialize
+        self.MineMap = [[] for _ in range(self.HEIGHT)]
+
+        # map data input
+        for i in range(self.HEIGHT):
+            for cell in input().split():
+                # cell: '?' if unknown, '.' if no mines nearby, '1'-'8' otherwise
+                self.MineMap[i].append(cell)
+
+
+    def update(self):
+        pass
+
+
+    def reveal_cell(self, coord):
+        #row, col = coordinate_to_rowcol(coord)
+        print("20 7 15 10")
+
+
+
+    def coordinate_to_rowcol(self, coord):
+        pass
+
+
+    def main(self):
+        while self.GameState == GameState.GAME:
+            self.input_data()
+            self.update()
+            self.print_Map()
+            self.reveal_cell((20, 7))
+            self.Turn += 1
+
+
+mine = Mine()
+mine.main()
