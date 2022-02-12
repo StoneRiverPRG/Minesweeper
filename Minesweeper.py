e8a5b5c15cbf6b3d452ba92421dc1b6268159d78
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2022-02-12 16:47:47 +0900

    print Map

--- Minesweeper.py
+++ Minesweeper.py
@@ -4,13 +4,18 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+width = 30
+height = 16
+mineCount = 99
 
 # game loop
 while True:
-    for i in range(16):
+    for i in range(height):
         for cell in input().split():
             # cell: '?' if unknown, '.' if no mines nearby, '1'-'8' otherwise
-            pass
+            print(cell, file=sys.stderr, end="")
+        print("", file=sys.stderr)
+
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
