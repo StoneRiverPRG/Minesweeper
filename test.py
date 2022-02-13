e4608f3aef88059b7de1b7190cb60b6baeb60127
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2022-02-13 22:25:49 +0900

    test

--- test.py
+++ test.py
@@ -11,3 +11,34 @@ if not (True and False):
     print("True and False")
 
 print(False and False)
+
+def return_int():
+    return
+
+print(return_int())
+# None
+
+def coordinate_to_rowcol(coord):
+    # *check legal position
+    if not len(coord) == 2:
+        return
+    x, y = coord
+    if not ((0 <= x < 16) and (0 <= y < 32)):
+        # ! if list index is out of range.
+        return
+
+    return (y, x)
+
+tp = (20, 20)
+print(coordinate_to_rowcol(tp))
+
+number_cell = list("12345678")
+print(number_cell)
+
+d = {}
+d["?"] = 1
+print(d)
+print(d.get("?"))
+if not d.get("?") == None:
+    d["?"] += 1
+print(d)
