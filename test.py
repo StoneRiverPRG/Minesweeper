3f46386432f5ef6748fbd3373802f13cc671e89a
StoneRiverPRG <6197619+StoneRiverPRG@users.noreply.github.com>
2022-02-13 18:34:42 +0900

    test.py追加

---
+++ test.py
@@ -0,0 +1,13 @@
+if not (False and False):
+    print("False and False")
+
+if not (True and True):
+    print("True and True")
+
+if not (False and True):
+    print("False and True")
+
+if not (True and False):
+    print("True and False")
+
+print(False and False)
